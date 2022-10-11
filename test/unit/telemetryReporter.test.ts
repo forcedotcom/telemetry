0f5786b9c2c602a926b0732241a0cb1183c5b238
mshanemc <shane.mclaughlin@salesforce.com>
2022-10-10 21:34:05 -0500

    test: windows homedir debug

--- test/unit/telemetryReporter.test.ts
+++ test/unit/telemetryReporter.test.ts
@@ -4,6 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+import * as os from 'os';
 import { SfdxConfigAggregator, Logger } from '@salesforce/core';
 import axios from 'axios';
 import { expect } from 'chai';
@@ -11,7 +12,7 @@ import * as sinon from 'sinon';
 import { AppInsights } from '../../src/appInsights';
 import { TelemetryReporter } from '../../src/telemetryReporter';
 
-describe('TelemetryReporter', () => {
+sdescribe('TelemetryReporter', () => {
   const key = 'foo-bar-123';
   const project = 'force-com-toolbelt';
 
@@ -46,6 +47,8 @@ describe('TelemetryReporter', () => {
     reporter.sendTelemetryException(new Error('testException'));
     expect(sendStub.calledOnce).to.be.true;
 
+    // eslint-disable-next-line no-console
+    console.log(`homedir is ${os.homedir()}`);
     // eslint-disable-next-line no-console
     console.log(sendStub.firstCall.args[0].exception.stack);
     expect(sendStub.firstCall.args[0].exception.stack).to.contain(AppInsights.GDPR_HIDDEN);
