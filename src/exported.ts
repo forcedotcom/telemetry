78596b20ea332df4cecbf194840c9a271c70441e
mshanemc <shane.mclaughlin@salesforce.com>
2023-10-31 11:15:04 -0500

    chore: dev-scripts changes

--- src/exported.ts
+++ src/exported.ts
@@ -4,8 +4,6 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-'use strict';
-
 import { TelemetryReporter } from './telemetryReporter';
 
 export * from './telemetryReporter';
